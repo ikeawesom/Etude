877c763afe83243eb66391f795dd267d0ae4997a
ikeawesom <ikelimjl@gmail.com>
2023-10-12 17:41:48 +0800

    utility component

---
+++ src/components/utils/PrimaryButton.js
@@ -0,0 +1,15 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+export default function PrimaryButton({ children, clickEvent }) {
+  return (
+    <TouchableOpacity
+      onPress={clickEvent}
+      className="px-8 py-4 rounded-lg shadow-md bg-sky-500 self-stretch"
+    >
+      <Text className="text-white text-2xl font-default-regular text-center">
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+}
