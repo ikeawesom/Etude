22a56f5007a8a72a8d53de673af71c808b335c77
ikeawesom <ikelimjl@gmail.com>
2023-10-12 17:42:23 +0800

    added theme context

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+
+export const ThemeContext = createContext(null);
+
+export function ThemeContextProvider({ children }) {
+  const [theme, setTheme] = useState("dark");
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useThemeContext() {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error(
+      "useThemeContext must be used within a ThemeContextProvider"
+    );
+  }
+  return context;
+}
+
+export function LightCheck() {
+  const { theme } = useThemeContext();
+  return theme === "light";
+}
