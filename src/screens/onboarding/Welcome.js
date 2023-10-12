d090f6a91e42d70f51921a9aa87e88bf1daf333f
ikeawesom <ikelimjl@gmail.com>
2023-10-12 23:37:21 +0800

    added loading screen

--- src/screens/onboarding/Welcome.js
+++ src/screens/onboarding/Welcome.js
@@ -56,15 +56,5 @@ export default function WelcomeScreen({ navigation }) {
         </View>
       </SafeCenter>
     );
-  return (
-    <SafeCenter styles={`${LightCheck() ? "bg-slate-100" : "bg-slate-800"}`}>
-      <Text
-        className={`${
-          LightCheck() ? "text-slate-800" : "text-slate-50"
-        } text-6xl font-default-regular text-center`}
-      >
-        Loading resources...
-      </Text>
-    </SafeCenter>
-  );
+  return <LoadingScreen />;
 }
