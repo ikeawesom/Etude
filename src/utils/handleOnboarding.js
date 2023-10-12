ecbdf454c95dab5d4d2cfb65d5d477a9a4d217c4
ikeawesom <ikelimjl@gmail.com>
2023-10-12 23:37:41 +0800

    modified onboarding functions

--- src/utils/handleOnboarding.js
+++ src/utils/handleOnboarding.js
@@ -1,10 +1,20 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getUserPrefs } from "./handlePreferences";
+
+export async function resetOnboarding() {
+  try {
+    await AsyncStorage.removeItem("ONBOARDED_STATUS");
+    return { error: null, status: true };
+  } catch (error) {
+    return { error: error, status: false };
+  }
+}
 
 export async function checkOnboard() {
   try {
     const value = await AsyncStorage.getItem("ONBOARDED_STATUS");
-    if (value !== null) return { error: null, status: true };
-    return { error: error, status: false };
+    if (value === null) return { error: null, status: false }; // first time user logs in
+    return { error: null, status: true }; // user completed onboarding
   } catch (error) {
     return { error: error, status: false };
   }
@@ -12,42 +22,10 @@ export async function checkOnboard() {
 
 export async function setOnboard() {
   try {
-    await AsyncStorage.setItem("ONBOARDED_STATUS", true);
+    await AsyncStorage.setItem("ONBOARDED_STATUS", "true");
+    const userPrefs = await getUserPrefs();
+    console.log("User onboarded successfully", userPrefs);
     return { error: null, status: true };
-  } catch (error) {
-    return { error: error, status: false };
-  }
-}
-
-export async function setAssistantType(type) {
-  try {
-    await AsyncStorage.setItem("ASSISTANT_TYPE", type);
-    return { error: null, status: true };
-  } catch (error) {
-    return { error: error, status: false };
-  }
-}
-
-export async function getAssistantName() {
-  const maleNames = ["Alex", "Max", "Leo", "Sam", "Kai"];
-  const femaleNames = ["Zoe", "Mia", "Eva", "Amy", "Ivy"];
-  try {
-    const assistantType = await AsyncStorage.getItem("ASSISTANT_TYPE");
-    if (assistantType !== null) {
-      const assistantName = await AsyncStorage.getItem("ASSISTANT_NAME");
-      if (assistantName === null) {
-        var name;
-        if (assistantType === "MALE") {
-          name = maleNames[Math.floor(Math.random() * maleNames.length)];
-        } else {
-          name = femaleNames[Math.floor(Math.random() * femaleNames.length)];
-        }
-        await AsyncStorage.setItem("ASSISTANT_NAME", name);
-        return { error: null, status: true, name: name };
-      }
-      return { error: null, status: true, name: assistantName };
-    }
-    return { error: error, status: false };
   } catch (error) {
     return { error: error, status: false };
   }
