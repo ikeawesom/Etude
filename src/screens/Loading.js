98541020c3e194a084eac6dbf0aa0d726fff7228
ikeawesom <ikelimjl@gmail.com>
2023-10-13 16:19:06 +0800

    imitate splash screen

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -1,18 +1,11 @@
-import { ext } from "react-native";
+import { Image, ext } from "react-native";
 import React from "react";
 import SafeCenter from "../components/SafeCenter";
-import { LightCheck } from "../contexts/ThemeContext";
 
 export default function LoadingScreen() {
   return (
-    <SafeCenter styles={`${LightCheck() ? "bg-slate-100" : "bg-slate-800"}`}>
-      <Text
-        className={`${
-          LightCheck() ? "text-slate-800" : "text-slate-50"
-        } text-3xl font-default-regular text-center`}
-      >
-        Loading resources...
-      </Text>
+    <SafeCenter styles="bg-slate-800">
+      <Image source={require("../../assets/icon.png")} />
     </SafeCenter>
   );
 }
