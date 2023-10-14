34ff51a4e92f81ed1bf0ae503ab7669dd60bdc62
Ike <ikelimjl@gmail.com>
2023-10-14 17:31:15 +0800

    load userdata on startup

--- src/screens/Navigations.js
+++ src/screens/Navigations.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Home, Events, Settings, Assignments } from ".";
+import { Home, Events, Settings, Assignments, LoadingScreen } from ".";
 import { StatusBar } from "expo-status-bar";
+import { initUserData } from "../utils/handleUserData";
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigations() {
-  return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Events" component={Events} />
-      <Tab.Screen name="Assignments" component={Assignments} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
-  );
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleUserData = async () => {
+      const { status } = await initUserData();
+      if (status) setLoaded(true);
+    };
+
+    handleUserData();
+  }, []);
+
+  if (loaded)
+    return (
+      <>
+        <StatusBar style="auto" />
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Events" component={Events} />
+          <Tab.Screen name="Assignments" component={Assignments} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </>
+    );
+  return <LoadingScreen />;
 }
