9ad20caa6fc0ee26696e7fec60149dc9c95b29a5
Ike <ikelimjl@gmail.com>
2023-10-14 17:44:14 +0800

    added events and assignment containers

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,67 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import AssignmentsContainer from "../components/AssignmentsContainer";
+import { getUserPrefs } from "../utils/handlePreferences";
+import LoadingScreen from "../screens/Loading";
+import { useThemeContext } from "../contexts/ThemeContext";
+import { ScrollView } from "react-native";
+import EventsContainer from "../components/EventsContainer";
 
-export default function Home() {
-  return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
-  );
+export default function Home({ navigation }) {
+  const { theme } = useThemeContext();
+  const [userPrefs, setUserPrefs] = useState({
+    name: "",
+    assistantType: "",
+    assistantName: "",
+  });
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleUserPrefs = async () => {
+      const res = await getUserPrefs();
+      if (res) {
+        setUserPrefs(res);
+        setLoaded(true);
+      }
+    };
+
+    handleUserPrefs();
+  }, []);
+
+  if (loaded)
+    return (
+      <SafeAreaView
+        className={`pt-10 ${
+          theme === "light" ? "bg-slate-100" : "bg-slate-800"
+        }`}
+      >
+        <ScrollView className={`p-10 gap-y-8 pt-0 `}>
+          <View>
+            <Text
+              className={`text-3xl font-default-regular text-center ${
+                theme === "dark" ? "text-slate-50" : "text-slate-800"
+              }`}
+            >
+              Hi there, <Text className="text-sky-500">{userPrefs.name}</Text>!
+              What can I do for you today?
+            </Text>
+          </View>
+          <View>
+            <EventsContainer navigation={navigation} />
+          </View>
+          <View>
+            <AssignmentsContainer navigation={navigation} />
+          </View>
+          <Text
+            className={`py-4 text-center font-default-semibold ${
+              theme === "dark" ? "text-slate-50" : "text-slate-800"
+            }`}
+          >
+            Created by <Text className="text-sky-500">Mentaicode</Text>
+          </Text>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  return <LoadingScreen />;
 }
