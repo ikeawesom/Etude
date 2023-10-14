24cb08ba098c9c73c333257127589d0965cb60e9
Ike <ikelimjl@gmail.com>
2023-10-14 17:30:55 +0800

    changed icon styles

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -1,11 +1,15 @@
-import { Image, ext } from "react-native";
+import { Image } from "react-native";
 import React from "react";
 import SafeCenter from "../components/SafeCenter";
 
 export default function LoadingScreen() {
   return (
     <SafeCenter styles="bg-slate-800">
-      <Image source={require("../../assets/icon.png")} />
+      <Image
+        source={require("../../assets/icon.png")}
+        style={{ height: 150, width: 150 }}
+        className="rounded-xl"
+      />
     </SafeCenter>
   );
 }
