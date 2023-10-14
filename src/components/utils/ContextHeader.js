e77cc06b696fe7fb3ac617eab3fc0648d6ef2d98
Ike <ikelimjl@gmail.com>
2023-10-14 21:10:19 +0800

    added context header

---
+++ src/components/utils/ContextHeader.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function ContextHeader({ title }) {
+  return (
+    <Text
+      className={`text-slate-50
+          text-2xl font-default-regular mb-4`}
+    >
+      {title}
+    </Text>
+  );
+}
