99bd7dae9ab6795006632da0aada9e79e1f1d482
ikeawesom <ikelimjl@gmail.com>
2023-10-15 16:36:54 +0800

    styled empty containers

--- src/components/AssignmentsContainer.js
+++ src/components/AssignmentsContainer.js
@@ -8,8 +8,14 @@ import { TouchableOpacity, Text, View } from "react-native";
 import ContextHeader from "./utils/ContextHeader";
 
 export default function AssignmentsContainer({ navigation }) {
-  const [assignments, setAssignments] = useState({});
+  const [assignments, setAssignments] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const empty =
+    assignments !== null
+      ? Object.keys(assignments).length === 0
+        ? true
+        : false
+      : false;
 
   useEffect(() => {
     const handleAssignments = async () => {
@@ -24,57 +30,56 @@ export default function AssignmentsContainer({ navigation }) {
     handleAssignments();
   }, [loaded]);
 
-  return (
-    <View>
-      <ContextHeader title="Assignments Due Soon" />
-      <View
-        className={`pb-3 ${
-          Object.keys(assignments).length === 0
-            ? "items-center justify-center min-h-[10vh]"
-            : ""
-        }`}
-      >
-        {Object.keys(assignments).length === 0 ? (
-          <Text className="font-montserrat-regular text-gray-400">
-            You're all caught up with work!
-          </Text>
-        ) : (
-          <View className="gap-y-2">
-            {Object.keys(assignments)
-              .slice(0, 3)
-              .map((item) => (
-                <View className={`p-4 rounded-xl bg-[#212530]`} key={item}>
-                  <View className="mb-1">
-                    <Text
-                      className={`text-base text-slate-50 font-default-bold ${assignments[item].color} self-start px-2 py-1 rounded-lg mb-2`}
-                    >
-                      {assignments[item].subject}
+  if (assignments)
+    return (
+      <View>
+        <ContextHeader title="Assignments Due Soon" />
+        <View
+          className={`items-center justify-center ${
+            empty ? "bg-dark-card rounded-lg min-h-[10vh]" : ""
+          }`}
+        >
+          {Object.keys(assignments).length === 0 ? (
+            <Text className="font-montserrat-regular text-gray-400">
+              You're all caught up with work!
+            </Text>
+          ) : (
+            <View className="gap-y-2">
+              {Object.keys(assignments)
+                .slice(0, 3)
+                .map((item) => (
+                  <View className={`p-4 rounded-xl bg-[#212530]`} key={item}>
+                    <View className="mb-1">
+                      <Text
+                        className={`text-base text-slate-50 font-default-bold ${assignments[item].color} self-start px-2 py-1 rounded-lg mb-2`}
+                      >
+                        {assignments[item].subject}
+                      </Text>
+                      <Text className="text-xl text-slate-50 font-default-semibold">
+                        {assignments[item].title}
+                      </Text>
+                    </View>
+                    <Text className="text-slate-300 font-montserrat-regular text-base">
+                      {assignments[item].desc.length > 30
+                        ? `${assignments[item].desc.substring(0, 30)}...`
+                        : assignments[item].desc}
                     </Text>
-                    <Text className="text-xl text-slate-50 font-default-semibold">
-                      {assignments[item].title}
+                    <Text className="text-slate-300 font-montserrat-regular text-sm mt-1">
+                      Due: {assignments[item].due}
                     </Text>
                   </View>
-                  <Text className="text-slate-300 font-montserrat-regular text-base">
-                    {assignments[item].desc.length > 30
-                      ? `${assignments[item].desc.substring(0, 30)}...`
-                      : assignments[item].desc}
-                  </Text>
-                  <Text className="text-slate-300 font-montserrat-regular text-sm mt-1">
-                    Due: {assignments[item].due}
-                  </Text>
-                </View>
-              ))}
-          </View>
-        )}
+                ))}
+            </View>
+          )}
+        </View>
+        <TouchableOpacity
+          className="self-end mt-2"
+          onPress={() => navigation.navigate("Assignments")}
+        >
+          <Text className={`text-primary font-default-medium text-lg`}>
+            View all
+          </Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        className="self-end"
-        onPress={() => navigation.navigate("Assignments")}
-      >
-        <Text className={`text-primary font-default-medium text-lg`}>
-          View all
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
+    );
 }
--- src/components/EventsContainer.js
+++ src/components/EventsContainer.js
@@ -4,8 +4,10 @@ import { View, Text, TouchableOpacity } from "react-native";
 import ContextHeader from "./utils/ContextHeader";
 
 export default function EventsContainer({ navigation }) {
-  const [events, setEvents] = useState({});
+  const [events, setEvents] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const empty =
+    events !== null ? (Object.keys(events).length === 0 ? true : false) : false;
 
   useEffect(() => {
     const handleEvents = async () => {
@@ -18,52 +20,52 @@ export default function EventsContainer({ navigation }) {
 
     handleEvents();
   }, [loaded]);
-  return (
-    <View>
-      <ContextHeader title="Upcoming Events" />
-      <View
-        className={`pb-3 ${
-          Object.keys(events).length === 0
-            ? "items-center justify-center min-h-[10vh]"
-            : ""
-        }`}
-      >
-        {Object.keys(events).length === 0 ? (
-          <Text className="font-montserrat-regular text-gray-400">
-            You're all caught up with work!
-          </Text>
-        ) : (
-          <View className="gap-y-2">
-            {Object.keys(events)
-              .slice(0, 3)
-              .map((item) => (
-                <View className={`p-4 rounded-xl bg-[#212530]`} key={item}>
-                  <View className="mb-1">
-                    <Text className="text-xl text-slate-50 font-default-semibold">
-                      {events[item].title}
+
+  if (events)
+    return (
+      <View>
+        <ContextHeader title="Upcoming Events" />
+        <View
+          className={`items-center justify-center ${
+            empty ? "bg-dark-card rounded-lg min-h-[10vh]" : ""
+          }`}
+        >
+          {empty ? (
+            <Text className="font-montserrat-regular text-gray-400">
+              Nothing for you to worry about here!
+            </Text>
+          ) : (
+            <View className="gap-y-2">
+              {Object.keys(events)
+                .slice(0, 3)
+                .map((item) => (
+                  <View className={`p-4 rounded-xl bg-[#212530]`} key={item}>
+                    <View className="mb-1">
+                      <Text className="text-xl text-slate-50 font-default-semibold">
+                        {events[item].title}
+                      </Text>
+                    </View>
+                    <Text className="text-slate-300 font-montserrat-regular text-base">
+                      {events[item].desc.length > 30
+                        ? `${events[item].desc.substring(0, 30)}...`
+                        : events[item].desc}
+                    </Text>
+                    <Text className="text-slate-300 font-montserrat-regular text-sm mt-1">
+                      Date: {events[item].due}
                     </Text>
                   </View>
-                  <Text className="text-slate-300 font-montserrat-regular text-base">
-                    {events[item].desc.length > 30
-                      ? `${events[item].desc.substring(0, 30)}...`
-                      : events[item].desc}
-                  </Text>
-                  <Text className="text-slate-300 font-montserrat-regular text-sm mt-1">
-                    Date: {events[item].due}
-                  </Text>
-                </View>
-              ))}
-          </View>
-        )}
+                ))}
+            </View>
+          )}
+        </View>
+        <TouchableOpacity
+          className="self-end mt-2"
+          onPress={() => navigation.navigate("Events")}
+        >
+          <Text className={`text-primary font-default-medium text-lg`}>
+            View all
+          </Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        className="self-end"
-        onPress={() => navigation.navigate("Events")}
-      >
-        <Text className={`text-primary font-default-medium text-lg`}>
-          View all
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
+    );
 }
